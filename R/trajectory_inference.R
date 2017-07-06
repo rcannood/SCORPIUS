990fd3beeeb493fb9029dc53e334a6c0417476d9
Robrecht Cannoodt <brechtus@gmail.com>
2017-07-06 14:53:00 +0200

    added number of threads explicitly.

--- R/trajectory_inference.R
+++ R/trajectory_inference.R
@@ -385,6 +385,7 @@ extract.modules <- function(x, ...) {
 #' @param num.permutations The number of permutations to test against for calculating the p-values (default: 0).
 #' @param ntree The number of trees to grow (default: 10000).
 #' @param mtry The number of variables randomly samples at each split (default: 1\% of features).
+#' @param num.threads Number of threads. Default is number of CPUs available.
 #' @param ... Extra parameters passed to ranger.
 #'
 #' @return a data frame containing the importance of each feature for the given time line
@@ -401,13 +402,13 @@ extract.modules <- function(x, ...) {
 #' space <- reduce.dimensionality(dist, ndim=2)
 #' traj <- infer.trajectory(space)
 #' gene.importances(expression, traj$time, num.permutations = 0)
-gene.importances <- function(x, time, num.permutations = 0, ntree = 10000, mtry = ncol(x) * .01, ...) {
+gene.importances <- function(x, time, num.permutations = 0, ntree = 10000, mtry = ncol(x) * .01, num.threads = NULL, ...) {
   data <- data.frame(x, XXXtimeXXX = time, check.names = F, stringsAsFactors = F)
   importance <- ranger::ranger(data = data, dependent.variable.name = "XXXtimeXXX", num.trees = ntree, mtry = mtry, importance = "impurity", ...)$variable.importance
   if (num.permutations > 0) {
     perms <- unlist(pbapply::pblapply(seq_len(num.permutations), function(i) {
       data$time <- sample(data$time)
-      ranger::ranger(data = data, dependent.variable.name = "XXXtimeXXX", num.trees = ntree, mtry = mtry, importance = "impurity", ...)$variable.importance
+      ranger::ranger(data = data, dependent.variable.name = "XXXtimeXXX", num.trees = ntree, mtry = mtry, importance = "impurity", num.threads = num.threads, ...)$variable.importance
     }))
     pvalue <- sapply(importance, function(x) mean(x < perms))
   } else {
