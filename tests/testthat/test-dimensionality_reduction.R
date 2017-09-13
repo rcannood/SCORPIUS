d9c86eac8087190b82b6f9f70ae3194839e0d2ab
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-09-13 10:20:35 +0200

    extend dimred tests

--- tests/testthat/test-dimensionality_reduction.R
+++ tests/testthat/test-dimensionality_reduction.R
@@ -5,6 +5,18 @@ test_that("With generated data", {
 
   dist <- correlation_distance(dataset$expression)
 
+  # testing for 1 dimension
+  space <- reduce_dimensionality(dist, ndim = 1, rescale = T)
+
+  expect_is( space, c("data.frame", "matrix") )
+  expect_equal( rownames(space), rownames(dist) )
+  expect_equal( colnames(space), paste0("Comp", seq_len(1)) )
+
+  ranges <- apply(space, 2, range)
+  expect_true( all.equal(1, max(apply(ranges, 2, diff))) )
+  expect_true( all(apply(ranges, 2, function(x) all.equal(0, mean(x)))) )
+
+  # testing for 2 dimensions
   space <- reduce_dimensionality(dist, ndim = 2, rescale = T)
 
   expect_is( space, c("data.frame", "matrix") )
@@ -15,9 +27,10 @@ test_that("With generated data", {
   expect_true( all.equal(1, max(apply(ranges, 2, diff))) )
   expect_true( all(apply(ranges, 2, function(x) all.equal(0, mean(x)))) )
 
-  space <- reduce_dimensionality(dist, ndim = 3, rescale = F)
+  # testing for 5 dimensions without rescaling
+  space <- reduce_dimensionality(dist, ndim = 5, rescale = F)
 
   expect_is( space, c("data.frame", "matrix") )
   expect_equal( rownames(space), rownames(dist) )
-  expect_equal( colnames(space), paste0("Comp", seq_len(3)) )
+  expect_equal( colnames(space), paste0("Comp", seq_len(5)) )
 })
