ba375a7076b2406f7f36309fad5ebe19c5797605
Robrecht Cannoodt <brechtus@gmail.com>
2018-06-12 19:22:20 +0200

    fix links

--- R/gene_importances.R
+++ R/gene_importances.R
@@ -3,7 +3,7 @@
 #' @description Calculates the feature importance of each column in \code{x} in trying to predict the time ordering.
 #'
 #' @param x A numeric matrix or data frame with \emph{M} rows (one per sample) and \emph{P} columns (one per feature).
-#' @param time A numeric vector containing the inferred time points of each sample along a trajectory as returned by \code{\link{infer.trajectory}}.
+#' @param time A numeric vector containing the inferred time points of each sample along a trajectory as returned by \code{\link{infer_trajectory}}.
 #' @param num_permutations The number of permutations to test against for calculating the p-values (default: 0).
 #' @param ntree The number of trees to grow (default: 10000).
 #' @param ntree_perm The number of trees to grow for each of the permutations (default: ntree / 10).
