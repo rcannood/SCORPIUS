904f03372419a858051a741bb4c34a67a4404945
Robrecht Cannoodt <rcannood@gmail.com>
2019-05-17 11:50:22 +0200

    fix error in R 3.6

--- R/extract_modules.R
+++ R/extract_modules.R
@@ -55,30 +55,15 @@ extract_modules <- function(x, time = NULL, suppress_warnings = FALSE, verbose =
   # determine mean module expression
   module_means <- do.call(cbind, tapply(feature_names, labels, function(fn) rowMeans(x[,fn,drop=FALSE])))
 
-  order_data <- function(z) {
-    if (ncol(z) <= 2) {
-      pct <- seq(0, 1, length.out = ncol(z))
-    } else if (ncol(z) == 3) {
-      pct <- reduce_dimensionality(t(z), "spearman", ndim = 1)[,1]
-      pct <- (pct - min(pct)) / (max(pct) - min(pct))
-    } else {
-      pct <- suppressWarnings(infer_trajectory(t(z), k = NULL)$time)
-    }
-    if (!is.null(time) && ncol(z) > 1 && stats::cor(stats::cor(time, z)[1,], pct) < 0) {
-      pct <- -pct
-    }
-    pct
-  }
-
   # reorder modules
-  module_time <- order_data(module_means)
+  module_time <- .extract_modules_order_data(module_means, time)
   labels <- order(order(module_time))[labels]
 
   # order features within one module according to a dimensionality reduction of the correlation distance
   modules <- map_df(sort(unique(labels)), function(l) {
     ix <- which(labels==l)
 
-    value <- order_data(x[,ix,drop=FALSE])
+    value <- .extract_modules_order_data(x[,ix,drop=FALSE], time)
     within_module_ordering <- percent_rank(value)
 
     tibble(
@@ -92,4 +77,22 @@ extract_modules <- function(x, time = NULL, suppress_warnings = FALSE, verbose =
 
   # return output
   modules
+}
+
+.extract_modules_order_data <- function(z, time) {
+  if (ncol(z) <= 2) {
+    pct <- seq(0, 1, length.out = ncol(z))
+  } else if (ncol(z) <= 5) {
+    pct <- reduce_dimensionality(t(z), "spearman", ndim = 1)[,1]
+    pct <- (pct - min(pct)) / (max(pct) - min(pct))
+  } else {
+    tz <- t(z)
+    fi <- apply(tz, 2, function(x) length(unique(x))) > 3
+    tz <- tz[, fi, drop = FALSE]
+    pct <- suppressWarnings(infer_trajectory(tz, k = NULL)$time)
+  }
+  if (!is.null(time) && ncol(z) > 1 && stats::cor(stats::cor(time, z)[1,], pct) < 0) {
+    pct <- -pct
+  }
+  pct
 }
