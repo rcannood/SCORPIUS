861b6f45745f79378fecba2c104e96fd7789e98f
Robrecht Cannoodt <rcannood@gmail.com>
2020-03-13 13:57:17 +0100

    rework dynwrap integration

--- R/dynwrap_integration.R
+++ R/dynwrap_integration.R
@@ -1,65 +1,67 @@
-#' Run scorpius using the dynwrap pipeline
+#' Run SCORPIUS using the dynwrap pipeline
 #'
 #' @param expression Expression matrix
 #' @param parameters Parameters
 #' @param priors Priors
 #' @param verbose Verbosity level
 #' @param seed Random seed
-#'
-#' @importFrom dynwrap wrap_data add_linear_trajectory add_timings add_dimred
-run_fun <- function(expression, priors, parameters, seed = NULL, verbose = 0)  {
-  if (!is.null(seed)) set.seed(seed)
+ti_scorpius_run_fun <- function(expression, priors, parameters, seed = NULL, verbose = 0)  {
+  if (requireNamespace("dynwrap", quietly = TRUE)) {
+    if (!is.null(seed)) set.seed(seed)
 
-  # TIMING: done with preproc
-  checkpoints <- list(method_afterpreproc = Sys.time())
+    # TIMING: done with preproc
+    checkpoints <- list(method_afterpreproc = Sys.time())
 
-  # REDUCE DIMENSIONALITY
-  space <- reduce_dimensionality(
-    x = expression,
-    dist = parameters$distance_method,
-    ndim = parameters$ndim
-  )
-
-  # INFER TRAJECTORY
-  traj <- infer_trajectory(
-    space,
-    k = parameters$k,
-    thresh = parameters$thresh,
-    maxit = parameters$maxit,
-    stretch = parameters$stretch,
-    smoother = parameters$smoother,
-    approx_points = 100
-  )
-
-  # TIMING: done with method
-  checkpoints$method_aftermethod <- Sys.time()
-
-  # SAVE OUTPUT
-  output <-
-    dynwrap::wrap_data(
-      cell_ids = names(traj$time)
-    ) %>%
-    dynwrap::add_linear_trajectory(
-      pseudotime = traj$time
-    ) %>%
-    dynwrap::add_timings(
-      timings = checkpoints
+    # REDUCE DIMENSIONALITY
+    space <- reduce_dimensionality(
+      x = expression,
+      dist = parameters$distance_method,
+      ndim = parameters$ndim
     )
 
-  # convert trajectory to segments
-  dimred_segment_points <- traj$path
-  dimred_segment_progressions <- output$progressions %>% select("from", "to", "percentage")
-
-  output <-
-    output %>%
-    dynwrap::add_dimred(
-      dimred = space,
-      dimred_segment_points = dimred_segment_points,
-      dimred_segment_progressions = dimred_segment_progressions,
-      connect_segments = TRUE
+    # INFER TRAJECTORY
+    traj <- infer_trajectory(
+      space,
+      k = parameters$k,
+      thresh = parameters$thresh,
+      maxit = parameters$maxit,
+      stretch = parameters$stretch,
+      smoother = parameters$smoother,
+      approx_points = 100
     )
 
-  output
+    # TIMING: done with method
+    checkpoints$method_aftermethod <- Sys.time()
+
+    # SAVE OUTPUT
+    output <-
+      dynwrap::wrap_data(
+        cell_ids = names(traj$time)
+      ) %>%
+      dynwrap::add_linear_trajectory(
+        pseudotime = traj$time
+      ) %>%
+      dynwrap::add_timings(
+        timings = checkpoints
+      )
+
+    # convert trajectory to segments
+    dimred_segment_points <- traj$path
+    dimred_segment_progressions <- output$progressions %>% select("from", "to", "percentage")
+
+    output <-
+      output %>%
+      dynwrap::add_dimred(
+        dimred = space,
+        dimred_segment_points = dimred_segment_points,
+        dimred_segment_progressions = dimred_segment_progressions,
+        connect_segments = TRUE
+      )
+
+    output
+  } else {
+    stop("ti_scorpius() requires dynwrap to be installed.")
+  }
 }
 
 #' Infer a trajectory using SCORPIUS
@@ -72,5 +74,5 @@ run_fun <- function(expression, priors, parameters, seed = NULL, verbose = 0)  {
 #' @export
 ti_scorpius <- dynwrap::create_ti_method_r(
   definition = system.file("dynwrap/definition.yml", package = "SCORPIUS"),
-  run_fun = run_fun
+  run_fun = ti_scorpius_run_fun
 )
